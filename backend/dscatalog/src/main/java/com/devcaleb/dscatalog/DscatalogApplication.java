e3811fdb0b3dc7017ba6862ceaa89741b08813aa
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-01-02 14:17:28 +0200

    Lombok added

--- backend/dscatalog/src/main/java/com/devcaleb/dscatalog/DscatalogApplication.java
+++ backend/dscatalog/src/main/java/com/devcaleb/dscatalog/DscatalogApplication.java
@@ -1,13 +1,20 @@
 package com.devcaleb.dscatalog;
 
+import com.devcaleb.dscatalog.entity.Category;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscatalogApplication {
+public class DscatalogApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatalogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category category = new Category(1);
+		System.out.println(category.getId());
+	}
 }
