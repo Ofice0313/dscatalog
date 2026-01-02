bdefbef3af9ab8b541017c51fb405191067c6def
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2026-01-02 22:34:15 +0200

    Auditoria

--- backend/dscatalog/src/main/java/com/devcaleb/dscatalog/entity/Category.java
+++ backend/dscatalog/src/main/java/com/devcaleb/dscatalog/entity/Category.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,4 +18,20 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
+    }
 }
